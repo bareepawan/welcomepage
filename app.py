94424e4a5b6eccd83790e5b2adf3874aced54a0c
Your NameYourclear <you@example.com>
2025-03-03 16:46:49 +0530

    app.py has been modified

--- app.py
+++ app.py
@@ -122,5 +122,5 @@ def login():
         conn.close()
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=10000)
