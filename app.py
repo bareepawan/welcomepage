3d4982b256404655884e8c560db0d8aeb5d4a742
Your NameYourclear <you@example.com>
2025-03-03 13:29:59 +0530

    port changed

--- app.py
+++ app.py
@@ -123,6 +123,4 @@ def login():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=10000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
